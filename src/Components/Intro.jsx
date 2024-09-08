ed1b01e8607a7188a9d1f659ac3b2d58af857fba
NaziaBeaghum <nazia.mk2@gmail.com>
2024-09-08 08:32:27 +0530

    seventh commit

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -13,7 +13,7 @@ const Intro = () => {
             <p style={{fontWeight:"bold"}}>Hi,Im  Nazia</p>
             <h2 style={{color:"white"}}>React<span className='content_span'>Developer</span></h2>
             <div className='btn_div'>
-              <a href='https://docs.google.com/document/d/e/2PACX-1vRia3GL_HkQaBEg-SyKZ9FNtcepYDSR5ZAY8voQTszAFBMJ5cgFcKdF0UCQFHFPIyFOhLENDezqMCm5/pub' className='home_btn'>Resume</a>
+              
               <button className='home_btn' ><Link to='/contact' >Contact </Link></button>
             </div>
 
